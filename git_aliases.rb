ab0c5b5e9f43cc72a8795baac675ea640e6b1027
Andrew Meyer <a.meyer@astronautics.com>
2013-08-02 11:31:11 -0500

    Add install script

---
+++ git_aliases.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+
+class GitAliasesInstallation
+  def initialize(root)
+    @root = root
+  end
+
+  def install_basics
+    puts "Installing git_aliases in '#{@root}'"
+    system("git", "config", "--global", "alias-config.alias-root", @root)
+  end
+
+  def uninstall_basics
+    puts "Uninstalling git_aliases"
+    system("git", "config", "--global", "--unset", "alias-config.alias-root")
+  end
+
+  def uninstall_all
+    uninstall_basics
+    system("git", "config", "--global", "--unset-all", "include.path", "#{Regexp.escape(@root)}/.*")
+  end
+
+  def install_all
+    install_basics
+    system("git", "config", "--global", "include.path", File.join(@root, "all.gitconfig"))
+  end
+end
+
+installation = GitAliasesInstallation.new(File.expand_path(File.dirname(__FILE__)))
+
+case ARGV[0]
+when "install"
+  installation.install_all
+when "uninstall"
+  installation.uninstall_all
+else
+  puts "Usage: #{$0} (install|uninstall)"
+end
