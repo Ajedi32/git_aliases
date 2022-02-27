77ab483f997d26e2cf9da9e3fde82b75a059e392
Andrew Meyer <andrewm.bpi@gmail.com>
2022-02-27 16:55:08 -0600

    Add docs to whoami alias

--- lib/git_aliases/commands/whoami.sh
+++ lib/git_aliases/commands/whoami.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+# See:
+# - https://git-scm.com/docs/git-config#Documentation/git-config.txt-username
+# - https://git-scm.com/book/en/v2/Git-Internals-Environment-Variables#_committing
+
 echo "Author: $(git config author.name || git config user.name) <${GIT_AUTHOR_EMAIL:-${EMAIL:-$(git config author.email || git config user.email)}}>"
 echo "Committer: $(git config committer.name || git config user.name) <${GIT_COMMITTER_EMAIL:-${EMAIL:-$(git config committer.email || git config user.email)}}>"
